bec6b2fdc398c8b43d0972bef2b3491e4fe1181d
manixcoder <pathakmanish86@gmail.com>
2022-02-01 14:16:13 +0530

    setup routes

--- resources/views/merchent/master.blade.php
+++ resources/views/merchent/master.blade.php
@@ -2,7 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('public/frontendAssets/css/aos.css')}}" rel="stylesheet">
