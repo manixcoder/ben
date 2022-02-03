5ba382fa14c41b6f7252c76c920fd8b2526d5096
manixcoder <pathakmanish86@gmail.com>
2022-02-03 17:18:55 +0530

    Create migration

--- database/migrations/2022_02_03_105144_create_food_dises_table.php
+++ database/migrations/2022_02_03_105144_create_food_dises_table.php
@@ -35,6 +35,6 @@ class CreateFoodDisesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('food_disees');
+        Schema::dropIfExists('food_dises');
     }
 }
