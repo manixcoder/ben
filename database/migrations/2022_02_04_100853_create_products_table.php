cff7b7979b579c2b3693c813e7ebda298e1bbb16
manixcoder <pathakmanish86@gmail.com>
2022-02-04 17:54:59 +0530

    table and products migrations

--- database/migrations/2022_02_04_100853_create_products_table.php
+++ database/migrations/2022_02_04_100853_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('merchent_id');
-            $table->string('product_name')->nullable();
+            $table->string('product_name')->nullable(); 
             $table->string('pro_category')->nullable();
             $table->string('pro_price')->nullable();
             $table->string('pro_discount')->nullable();
